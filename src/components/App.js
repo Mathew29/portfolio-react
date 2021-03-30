96481d0eb350ae06e4160f4aae4778ec7866322b
Mathew <mwakre29@gmail.com>
2021-03-29 20:35:27 -0700

    import hero file and add context info

--- src/components/App.js
+++ src/components/App.js
@@ -1,12 +1,38 @@
-import "./App.css";
+import React, { useState, useEffect } from "react";
+import Hero from "./Hero/Hero";
 
+import "./App.css";
 import { PortfolioProvider } from "../context/context";
 
+import {
+  aboutData,
+  contactData,
+  footerData,
+  heroData,
+  projectData,
+} from "../data/data";
+
 function App() {
+  const [hero, setHero] = useState({});
+  const [about, setAbout] = useState({});
+  const [projects, setProjects] = useState({});
+  const [contact, setContact] = useState({});
+  const [footer, setFooter] = useState({});
+
+  useEffect(() => {
+    setHero({ ...heroData });
+    setAbout({ ...aboutData });
+    setProjects({ ...projectData });
+    setContact({ ...contactData });
+    setFooter({ ...footerData });
+  }, []);
+
   return (
-    <div className="App">
-      <p></p>
-    </div>
+    <PortfolioProvider value={{ hero, about, projects, contact, footer }}>
+      <div>
+        <Hero />
+      </div>
+    </PortfolioProvider>
   );
 }
 
