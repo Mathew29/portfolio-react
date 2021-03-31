0923f80b78f9ec0613a2ac013fae9c035cccc5de
Mathew <mwakre29@gmail.com>
2021-03-31 14:09:16 -0700

    add content

--- src/components/Hero/Hero.js
+++ src/components/Hero/Hero.js
@@ -1,7 +1,7 @@
 import React, { useContext, useState, useEffect } from "react";
 
-import styles from "./Hero.css";
 import PortfolioContext from "../../context/context";
+import styles from "./Hero.module.css";
 
 const Header = () => {
   const { hero } = useContext(PortfolioContext);
@@ -12,10 +12,16 @@ const Header = () => {
   console.log("subtitle ", subtitle);
   console.log("cta ", cta);
   return (
-  <section className={styles.Container}>
-    <h1 className={styles.Title}></h1>
-    <p className={styles.Cta}></p>
-  </section>;)
+    <section className={styles.Container}>
+      <h1 className={styles.Title}>
+        {title || "Hi, my name is"}{" "}
+        <span className={styles.MainTextColor}>{name}</span>
+        <br />
+        {subtitle}
+      </h1>
+      <p className={styles.Cta}>paragrah</p>
+    </section>
+  );
 };
 
 export default Header;
