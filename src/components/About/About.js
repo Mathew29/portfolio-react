fedd5512bcf99b0c6296596585bbeb455bc13836
Mathew <mwakre29@gmail.com>
2021-04-15 21:24:16 -0700

    add classes

--- src/components/About/About.js
+++ src/components/About/About.js
@@ -1,5 +1,6 @@
 import React, { useContext } from "react";
 
+import styles from "./About.module.css";
 import PortfolioContext from "../../context/context";
 
 const About = () => {
@@ -7,15 +8,22 @@ const About = () => {
   const { img, paragraphOne, paragraphTwo, paragraphThree, resume } = about;
 
   return (
-    <section className="about">
-      <div className="about-wrapper">
-        <div className="img-wrapper">
-          <img src="img" alt="image" />
+    <section className={styles.About}>
+      <div className={styles.AboutWrapper}>
+        <div className={styles.ImgWrapper}>
+          <img src={img} alt="pic" />
         </div>
-        <div className="paragraph-wrapper">
-          <p className="p1">{paragraphOne || null}</p>
-          <p className="p2">{paragraphTwo}</p>
-          <p className="p3">{paragraphThree}</p>
+        <div className={styles.ParagraphWrapper}>
+          <p className={styles.P1}>{paragraphOne || null}</p>
+          <p className={styles.P2}>{paragraphTwo || null}</p>
+          <p className={styles.P3}>{paragraphThree || null}</p>
+          {resume && (
+            <div className={styles.ButtonWrapper}>
+              <a className={styles.Button} href="/">
+                {resume}
+              </a>
+            </div>
+          )}
         </div>
       </div>
     </section>
