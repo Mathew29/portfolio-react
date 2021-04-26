d01ab06211da897e2bf00e2f3cce726e5e24b428
Mathew <mwakre29@gmail.com>
2021-04-25 18:29:55 -0700

    add more class names and divs

--- src/components/About/About.js
+++ src/components/About/About.js
@@ -10,20 +10,27 @@ const About = () => {
   return (
     <section className={styles.About}>
       <div className={styles.AboutWrapper}>
-        <div className={styles.ImgWrapper}>
-          <img src={img} alt="pic" />
-        </div>
-        <div className={styles.ParagraphWrapper}>
-          <p className={styles.P1}>{paragraphOne || null}</p>
-          <p className={styles.P2}>{paragraphTwo || null}</p>
-          <p className={styles.P3}>{paragraphThree || null}</p>
-          {resume && (
-            <div className={styles.ButtonWrapper}>
-              <a className={styles.Button} href="/">
-                {resume}
-              </a>
+        <h1 className={styles.Title}>About</h1>
+        <div className={styles.AboutRow}>
+          <div className={styles.AboutCol}>
+            <div className={styles.ImgWrapper}>
+              <img src={img} alt="pic" />
             </div>
-          )}
+          </div>
+          <div className={styles.AboutCol}>
+            <div className={styles.InfoWrapper}>
+              <p className={styles.InfoText}>{paragraphOne || null}</p>
+              <p className={styles.InfoText}>{paragraphTwo || null}</p>
+              <p className={styles.InfoText}>{paragraphThree || null}</p>
+              {resume && (
+                <div className={styles.ButtonWrapper}>
+                  <a className={styles.Button} href="/">
+                    {resume}
+                  </a>
+                </div>
+              )}
+            </div>
+          </div>
         </div>
       </div>
     </section>
