1bc045d4e85f4564bd82f1e4a83d01345121f187
Mathew <mwakre29@gmail.com>
2021-05-26 16:08:43 -0700

    reformat for scss

--- src/components/About/About.js
+++ src/components/About/About.js
@@ -1,6 +1,6 @@
 import React, { useContext } from "react";
 
-import styles from "./About.module.css";
+import styles from "./About.scss";
 import PortfolioContext from "../../context/context";
 
 const About = () => {
@@ -8,28 +8,26 @@ const About = () => {
   const { img, paragraphOne, paragraphTwo, paragraphThree, resume } = about;
 
   return (
-    <section className={styles.About}>
-      <div className={styles.AboutWrapper}>
-        <h1 className={styles.Title}>About</h1>
-        <div className={styles.AboutRow}>
-          <div className={styles.AboutCol}>
-            <div className={styles.ImgWrapper}>
-              <img src={img} alt="pic" />
-            </div>
+    <section className="about-wrapper">
+      <h1 className="title">About Me</h1>
+      <div className="about-row">
+        <div className="about-col">
+          <div className="img-wrapper">
+            <img src={img} alt="pic" />
           </div>
-          <div className={styles.AboutCol}>
-            <div className={styles.InfoWrapper}>
-              <p className={styles.InfoText}>{paragraphOne || null}</p>
-              <p className={styles.InfoText}>{paragraphTwo || null}</p>
-              <p className={styles.InfoText}>{paragraphThree || null}</p>
-              {resume && (
-                <div className={styles.ButtonWrapper}>
-                  <a className={styles.Button} href="/">
-                    {resume}
-                  </a>
-                </div>
-              )}
-            </div>
+        </div>
+        <div className="about-col">
+          <div className="info-wrapper">
+            <p className="info-text">{paragraphOne || null}</p>
+            <p className="info-text">{paragraphTwo || null}</p>
+            <p className="info-text">{paragraphThree || null}</p>
+            {resume && (
+              <div className={styles.ButtonWrapper}>
+                <a className={styles.Button} href="/">
+                  {resume}
+                </a>
+              </div>
+            )}
           </div>
         </div>
       </div>
